6483e0be1b68d0c1d14d6afb349da8a60fac4a68
webuser <ryan@virtuecenter.com>
2014-04-22 03:15:29 -0400

    minor changes

--- available/InputHidden.php
+++ available/InputHidden.php
@@ -5,6 +5,9 @@ class InputHidden {
     public function render ($field) {
         $field['attributes']['type'] = 'hidden';
         $field['attributes']['name'] = $field['marker'] . '[' . $field['name'] . ']';
+        if (isset($field['data'])) {
+	        $field['attributes']['value'] = $field['data'];
+	    }
         return $this->fieldService->tag($field, 'input', $field['attributes']);
     }
 }
